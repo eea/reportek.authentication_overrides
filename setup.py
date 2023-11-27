2796248c3c97a4c6d6d8d42c7ad902cb220e51d3
Olimpiu Rob <olimpiu.rob@gmail.com>
2023-11-27 16:52:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -35,6 +35,5 @@ setup(name='reportek.authentication_overrides',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"]
+      setup_requires=["PasteScript"]
       )
